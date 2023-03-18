bf16cb4a15145882d3c3f04049315f14018f8bd7
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-18 10:53:48 +0000

    1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ my_tuple = 0,
 zero_tuple = tuple()
 my_set = {0}
 zero_set = set()
-my_dict = {0:0}
+my_dict = {0: 0}
 zero_dict = {}
 print('bool_true -', str(bool_true))
 print('bool_false -', str(bool_false))
@@ -47,7 +47,7 @@ my_list = [0]
 my_tuple = 0,
 my_set = {0}
 my_frozenset = frozenset()
-my_dict = {0:0}
+my_dict = {0: 0}
 zero_list = list()
 print('string -', frozenset(string))
 print('my_list -', frozenset(my_list))
@@ -58,20 +58,18 @@ print('my_dict -', frozenset(my_dict))
 print('zero_list -', frozenset(zero_list))
 
 print('\nПреобразования в словарь')
-my_list = ['01', ["1",2], (2,"3"), {3.3,4},
- frozenset({5,6})]
-my_tuple = ('01', ["1",2], (2,"3"), {3.3,4},
- frozenset({5,6}))
-my_set = {'01', (2,"3"), frozenset({5,6})}
+my_list = ['01', ["1", 2], (2, "3"), {3.3, 4}, frozenset({5, 6})]
+my_tuple = ('01', ["1", 2], (2, "3"), {3.3, 4}, frozenset({5, 6}))
+my_set = {'01', (2, "3"), frozenset({5, 6})}
 print(dict(my_list))
 print(dict(my_tuple))
 print(dict(my_set))
 
-my_dict = dict(string = 'string', number = 0,
- list = [])
+my_dict = dict(string='string', number=0, list=[])
 print(my_dict)
 
 print('\nЗадача "Стартапер"')
 my_basket = {}
-my_basket[input()] = int(input())
+ware_1 = input('Введите продукт:\n')
+my_basket[ware_1] = int(input('Введите стоимость выбранного Вами продукта:\n'))
 print(my_basket)
